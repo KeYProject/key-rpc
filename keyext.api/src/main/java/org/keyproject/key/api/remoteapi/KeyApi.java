2e406dca5ff8e6712f8926c077724e85d168e0b2
Alexander Weigl <weigl@kit.edu>
2023-10-29 16:45:43 +0100

    working on a working minimal version

--- keyext.api/src/main/java/org/keyproject/key/api/remoteapi/KeyApi.java
+++ keyext.api/src/main/java/org/keyproject/key/api/remoteapi/KeyApi.java
@@ -6,6 +6,6 @@ package org.keyproject.key.api.remoteapi;
 /**
  * The combined interface that is provided by KeY.
  */
-public interface KeyApi
-        extends ExampleApi, MetaApi, ServerManagement, ProofApi, ProofTreeApi, GoalApi, EnvApi {
+public interface KeyApi extends ExampleApi, MetaApi, ServerManagement, ProofApi, ProofTreeApi,
+        GoalApi, EnvApi, ProofLoadApi {
 }
